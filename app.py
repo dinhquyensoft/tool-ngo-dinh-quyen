b25d7fa8a927cb564990e27fd6a07e847fc232ab
dinhquyensoft <dinhq4610@gmail.com>
2026-01-09 17:04:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,11 @@ st.set_page_config(page_title="Đóng dấu ảnh - Ngô Đình Quyền", layout
 
 # Tiêu đề gốc
 
-st.markdown("<h1 style='text-align: center;'>GIẢI PHÁP ĐỊNH VỊ THƯƠNG HIỆU HÌNH ẢNH</h1>", unsafe_allow_html=True)
-
+st.markdown("""
+    <h1 style='text-align: center; color: #D4AF37; text-shadow: 2px 2px 4px #000000;'>
+        NGÔ ĐÌNH QUYỀN | GIẢI PHÁP ĐỊNH VỊ THƯƠNG HIỆU HÌNH ẢNH
+    </h1>
+""", unsafe_allow_html=True)
 
 
 # BƯỚC 1: CHỌN LOGO
